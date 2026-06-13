fde17f16754d2822f6b98d3f3be70cb7eefd19a8
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-12 21:05:57 -0400

    iniciando proyecto

---
+++ src/main/java/cl_duoc/ms_promociones/MsPromocionesApplication.java
@@ -0,0 +1,13 @@
+package cl_duoc.ms_promociones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPromocionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPromocionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl_duoc/ms_promociones/MsPromocionesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl_duoc.ms_promociones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPromocionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
